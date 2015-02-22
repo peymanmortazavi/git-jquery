3ca1a0e162396eedb9324bec44c992d4e0c58e4a
Peyman Mortazavi <pmortazavi@cailmobility.com>
2015-02-21 17:47:04 -0700

    build

--- js/my/events.js
+++ js/my/events.js
@@ -3,5 +3,20 @@ function myEvents(){
     // display a list of events belonging to me in #list
     // e.g., https://api.github.com/users/doubleshow/events
 
-    alert('todo')
+    $.get("https://api.github.com/users/peymanmortazavi/events", github, function(data){
+		
+		console.log(data);
+		
+		$.get("/git-jquery/templates/eventList.jade", function(template) {
+
+            // render the template
+            var html = jade.render(template, {items: data})            
+
+            // assign the rendered html to the dom element whose id is #list
+            $("#list").html(html)
+
+
+        })
+		
+	});
 }
